3a871fc00f3bf36f7a967561e61b4f75b239d16a
Choejeonghun <saa00123@naver.com>
2023-06-26 16:58:47 +0900

    fix: eslint prettier

--- pokemon-card/src/index.jsx
+++ pokemon-card/src/index.jsx
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const rootElement = document.getElementById('root');
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  rootElement
+  document.getElementById("root"),
 );
-
-reportWebVitals(console.log); // 콜백 함수 전달
-
