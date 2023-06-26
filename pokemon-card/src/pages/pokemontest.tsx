27262e14c1418aac9376a60ac7ac348af440cbc7
김혜진 <khj000217@naver.com>
2023-06-26 16:09:50 +0900

    feat:to get refactoring

--- pokemon-card/src/pages/pokemontest.tsx
+++ pokemon-card/src/pages/pokemontest.tsx
@@ -1,49 +1,41 @@
-// import React, { useState, useEffect } from 'react';
-// import styled from 'styled-components';
-// import pokemon from 'pokemontcgsdk';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import pokemon from 'pokemontcgsdk';
 
-// const Image = styled.div`
-//   width: 10rem;
-//   height: 20rem;
-//   background-repeat: none;
-//   background-image: ${(url) => `url(${url})`};
-// `;
+const Image = styled.div`
+  width: 10rem;
+  height: 20rem;
+  background-repeat: none;
+  background-image: ${(url) => `url(${url})`};
+`;
 
-// function PokemonTest() {
-//   const [results, SetResults] = useState([]);
-//   pokemon.configure({ apiKey: process.env.REACT_APP_POKEMON_API_KEY });
-//   useEffect(() => {
-//     // id로 카드 찾기
-//     pokemon.card.find('base2-1').then((card) => {
-//       console.log(card);
-//     });
+function PokemonTest() {
+  const [results, SetResults] = useState([]);
+  pokemon.configure({ apiKey: process.env.REACT_APP_POKEMON_API_KEY });
+  useEffect(() => {
+    // id로 카드 찾기
+    pokemon.card.find('base2-1').then((card) => {
+      console.log(card);
+    });
 
-//     // q 파라미터로 카드 찾기
-//     pokemon.card.where({ q: 'name:Charizard' }).then((result) => {
-//       SetResults(result);
-//     });
-//   }, []);
+    // q 파라미터로 카드 찾기
+    pokemon.card.where({ q: 'name:Charizard' }).then((result) => {
+      SetResults(result);
+    });
+  }, []);
 
-//   console.log('results : ', results);
+  console.log('results : ', results);
 
-//   results.map((result) => {
-//     console.log('img result : ', result.images);
-//     return (
-//       <>
-//         <Image>{result.images.small}</Image>
-//       </>
-//     );
-//   });
+  results.map((result) => {
+    console.log('img result : ', result.images);
+    return (
+      <>
+        <Image>{result.images.small}</Image>
+      </>
+    );
+  });
 
-//   return <div></div>;
-// }
-
-// export default PokemonTest;
-
-import React from 'react';
-
-function pokemontest() {
-  return <div>hello</div>;
+  return <div></div>;
 }
 
-export default pokemontest;
+// export default PokemonTest;
