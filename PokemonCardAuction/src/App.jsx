20b71da60c7271a600ee358ac2adb71440533a15
Choejeonghun <saa00123@naver.com>
2023-07-06 20:21:06 +0900

    fix: filename change

--- PokemonCardAuction/src/App.jsx
+++ PokemonCardAuction/src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Dashboard from "./pages/dashboard";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
