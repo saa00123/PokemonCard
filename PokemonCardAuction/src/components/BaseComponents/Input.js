c2add320cdfbd70f08b68d99883fd80e9a09ffd9
Choejeonghun <saa00123@naver.com>
2023-07-08 22:27:21 +0900

    feat: Input component

---
+++ PokemonCardAuction/src/components/BaseComponents/Input.js
@@ -0,0 +1,60 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const StyledInput = styled.input`
+  color: ${(props) => props.color};
+  font-size: ${(props) => props.fontSize};
+  font-weight: ${(props) => props.fontWeight};
+  border: ${(props) => props.border};
+  border-radius: ${(props) => props.borderRadius};
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  margin: ${(props) => props.margin};
+  padding: ${(props) => props.padding};
+`;
+
+const Input = (props) => {
+  const { color, fontSize, fontWeight, border, borderRadius, width, height, margin, padding, ...rest } = props;
+
+  return (
+    <StyledInput
+      color={color}
+      fontSize={fontSize}
+      fontWeight={fontWeight}
+      border={border}
+      borderRadius={borderRadius}
+      width={width}
+      height={height}
+      margin={margin}
+      padding={padding}
+      {...rest}
+    />
+  );
+};
+
+Input.propTypes = {
+  color: PropTypes.string,
+  fontSize: PropTypes.string,
+  fontWeight: PropTypes.string,
+  border: PropTypes.string,
+  borderRadius: PropTypes.string,
+  width: PropTypes.string,
+  height: PropTypes.string,
+  margin: PropTypes.string,
+  padding: PropTypes.string,
+};
+
+Input.defaultProps = {
+  color: "black",
+  fontSize: "1rem",
+  fontWeight: "normal",
+  border: "1px solid #ccc",
+  borderRadius: "4px",
+  width: "100%",
+  height: "40px",
+  margin: "0",
+  padding: "8px",
+};
+
+export default Input;
