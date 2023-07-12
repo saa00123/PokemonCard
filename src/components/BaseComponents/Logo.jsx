45d22659d3061fc93bdf004ad4f7097291a8e539
Choejeonghun <saa00123@naver.com>
2023-07-12 21:35:09 +0900

    feat: header

--- src/components/BaseComponents/Logo.jsx
+++ src/components/BaseComponents/Logo.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import styled from "styled-components";
 
-const Logo = () => (
-  <div>
-    <h1>Pokemon Card</h1>
-  </div>
-);
+const LogoContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 9.375rem;
+  height: 6.25rem;
+  font-size: 1.5rem;
+  font-weight: bold;
+  text-align: center;
+`;
+
+const Logo = () => <LogoContainer>PGA</LogoContainer>;
 
 export default Logo;
--- src/pages/main/Auction.jsx
+++ src/pages/main/Auction.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "../../components/BaseComponents/Header";
 
 function Auction() {
-  return <div>Auction</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default Auction;
