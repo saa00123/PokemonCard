e9bce973c2691197f73db358be5f5b647628749e
Choejeonghun <saa00123@naver.com>
2023-07-19 22:59:38 +0900

    feat: SignUpComplete responsive

--- src/pages/Login/SignUpComplete.jsx
+++ src/pages/Login/SignUpComplete.jsx
@@ -18,6 +18,7 @@ function SignUpComplete() {
       alignitems="center"
       padding="7.625rem 0 0 0"
       width="100vw"
+      notebookpadding="5.875rem 0 0 0"
     >
       <Div
         className="SignUpGif"
@@ -28,6 +29,9 @@ function SignUpComplete() {
         height="20.125rem"
         margin="0 0 2.938rem 0"
         border="solid 1px black"
+        notebookwidth="18.188rem"
+        notebookheight="15.5rem"
+        notebookmargin="0 0 2.25rem 0"
       >
         gif 추가
       </Div>
@@ -41,6 +45,9 @@ function SignUpComplete() {
         color={Gray2}
         fontSize="3rem"
         fontWeight="bold"
+        notebookwidth="39.875rem"
+        notebookheight="5.563rem"
+        notebookfontSize="2.5rem"
       >
         회원가입이 완료되었습니다.
       </Div>
@@ -53,6 +60,9 @@ function SignUpComplete() {
         borderradius="1rem"
         border="none"
         onClick={() => navigate("/Login")}
+        notebookwidth="9.375rem"
+        notebookheight="3.75rem"
+        notebookfontSize="1rem"
       >
         로그인하러 가기
       </Button>
