e2fc40ca2634165ce9539f8097de82ebefad1f60
김혜진 <khj000217@naver.com>
2023-07-20 16:51:56 +0900

    feat:SetupPassword 반응형 추가

--- src/pages/Login/ResetPassword.jsx
+++ src/pages/Login/ResetPassword.jsx
@@ -25,8 +25,8 @@ function ResetPassword() {
         className="ResetContainer"
         display="flex"
         flexdirection="column"
-        width="40.625rem"
-        height="46.064rem"
+        width="fit-content"
+        height="fit-content"
         margin="3.063rem 0 0 0"
       >
         <Div
@@ -36,6 +36,9 @@ function ResetPassword() {
           color={Gray2}
           fontsize="2rem"
           fontWeight="bold"
+          notebookwidth="12.5rem"
+          notebookheight="2.875rem"
+          notebookfontsize="1.75rem"
         >
           비밀번호 재설정
         </Div>
@@ -49,6 +52,8 @@ function ResetPassword() {
           height="25rem"
           boxshadow="4px 4px 20px 6px rgba(0,0,0,0.25)"
           backgroundcolor={Default}
+          notebookwidth="27.125rem"
+          notebookheight="19.25rem"
         >
           <Div className="PasswordContainer" display="flex" margin="0 0 0.5rem 0">
             <Div
@@ -59,10 +64,13 @@ function ResetPassword() {
               height="3.75rem"
               textalign="left"
               fontsize="1.625rem"
+              notebookwidth="8.125rem"
+              notebookheight="2.5rem"
+              notebookfontsize="1.25rem"
             >
               비밀번호
             </Div>
-            <Div display="flex" flexdirection="column" alignitems="center" width="21.875rem" height="5.25rem">
+            <Div display="flex" flexdirection="column" width="fit-content" height="fit-content">
               <Input
                 placeholder="비밀번호를 입력하세요."
                 padding="0 0 0 1.313rem"
@@ -70,6 +78,11 @@ function ResetPassword() {
                 height="3.75rem"
                 fontsize="1.625rem"
                 borderradius="15px"
+                notebookwidth="14.375rem"
+                notebookheight="2.5rem"
+                notebookfontsize="1rem"
+                notebookborderradius="0.625rem"
+                notebookpadding="0 0 0 0.863rem"
               />
               <Div
                 className="PasswordWarning"
@@ -77,8 +90,14 @@ function ResetPassword() {
                 alignitems="center"
                 width="21.438rem"
                 height="1.5rem"
+                boxsizing="border-box"
                 color={Gray1}
+                padding="0 0 0 1.313rem"
                 fontsize="0.75rem"
+                notebookwidth="10rem"
+                notebookheight="1.25rem"
+                notebookfontsize="0.625rem"
+                notebookpadding="0 0 0 0.863rem"
               >
                 8자 이상, 숫자, 영어, 특수분자 포함
               </Div>
@@ -93,17 +112,13 @@ function ResetPassword() {
               height="3.75rem"
               textalign="left"
               fontsize="1.625rem"
+              notebookwidth="8.125rem"
+              notebookheight="2.5rem"
+              notebookfontsize="1.25rem"
             >
               비밀번호 확인
             </Div>
-            <Div
-              display="flex"
-              flexdirection="column"
-              alignitems="center"
-              width="21.875rem"
-              height="5.25rem"
-              margin="0 0 2.688rem 0"
-            >
+            <Div display="flex" flexdirection="column" width="fit-content" height="fit-content" margin="0 0 2.688rem 0">
               <Input
                 placeholder="비밀번호를 다시 입력하세요."
                 padding="0 0 0 1.313rem"
@@ -111,6 +126,11 @@ function ResetPassword() {
                 height="3.75rem"
                 fontsize="1.625rem"
                 borderradius="15px"
+                notebookwidth="14.375rem"
+                notebookheight="2.5rem"
+                notebookfontsize="1rem"
+                notebookborderradius="0.625rem"
+                notebookpadding="0 0 0 0.863rem"
               />
               <Div
                 className="PasswordCheckWarning"
@@ -118,8 +138,14 @@ function ResetPassword() {
                 alignitems="center"
                 width="21.438rem"
                 height="1.5rem"
+                boxsizing="border-box"
+                padding="0 0 0 1.313rem"
                 color={Gray1}
                 fontsize="0.75rem"
+                notebookwidth="10rem"
+                notebookheight="1.25rem"
+                notebookfontsize="0.625rem"
+                notebookpadding="0 0 0 0.863rem"
               >
                 비밀번호가 일치하지 않습니다.
               </Div>
@@ -133,6 +159,10 @@ function ResetPassword() {
             fontWeight="bold"
             borderradius="1rem"
             border="none"
+            notebookwidth="8.438rem"
+            notebookheight="3.125rem"
+            notebookfontsize="1rem"
+            notebookborderradius="10px"
           >
             비밀번호 변경
           </Button>
