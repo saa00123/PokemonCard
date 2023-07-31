fc4b33c6ca41b30069a685d74f825a309590df64
김혜진 <khj000217@naver.com>
2023-08-01 00:27:03 +0900

    design:smallendauction 반응형 완료

--- src/components/ImageComponents/SmallEndAuction.jsx
+++ src/components/ImageComponents/SmallEndAuction.jsx
@@ -33,58 +33,72 @@ const PreviewImage = styled.div`
   }
 `;
 
+const DetailContainer = styled.div`
+  position: relative;
+  height: 8rem;
+  /* border: 1px solid yellowgreen; */
+  @media (min-width: 1024px) and (max-width: 1440px) {
+    width: fit-content;
+    height: 6rem;
+  }
+`;
+
 const PreviewHeader = styled.div`
   width: 18.75rem;
-  height: 3.5rem;
+  height: fit-content;
+  max-height: 3.5rem;
   font-size: 1.25rem;
   text-align: left;
   word-wrap: break-word;
+  /* border: 1px solid grey; */
 
   @media (min-width: 1024px) and (max-width: 1440px) {
     width: 12.5rem;
-    height: 2.876rem;
+    height: fit-content;
+    max-height: 2.876rem;
     font-size: 0.875rem;
     font-weight: normal;
   }
 `;
 
-const PreviewAuctionContainer = styled.div`
-  display: flex;
-  width: 18.75rem;
-  height: 3.438rem;
-  margin-top: 1.063rem;
-  /* border: 1px solid orange; */
-
+const DeadLine = styled.div`
+  margin-top: 0.5rem;
+  color: ${Color({ color: "Gray2" })};
+  font-size: 0.875rem;
   @media (min-width: 1024px) and (max-width: 1440px) {
-    width: 12.5rem;
-    height: 2.626rem;
-    margin-top: 0.5rem;
+    margin-top: 0.2rem;
+    font-size: 0.7rem;
   }
 `;
 
 const PreviewDetailContainer = styled.div`
-  width: "fit-content";
-  height: 3.438rem;
-  float: ${(props) => props.float};
-  margin-left: ${(props) => props.marginleft};
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  display: flex;
+  width: fit-content;
+  height: 2.188rem;
+  align-items: flex-end;
   /* border: 1px solid black; */
 
   @media (min-width: 1024px) and (max-width: 1440px) {
-    width: fit-content;
-    height: fit-content;
+    /* width: fit-content;
+    height: fit-content; */
   }
 `;
 
 const DetailHeader = styled.div`
   width: 4.063rem;
-  height: 1.25rem;
+  height: 1.2rem;
   font-size: 0.875rem;
   text-align: left;
+
   /* border: 1px solid red; */
 
   @media (min-width: 1024px) and (max-width: 1440px) {
     width: 2.75rem;
     height: 0.938rem;
+    /* right: 2.75rem; */
     font-size: 0.625rem;
   }
 `;
@@ -121,26 +135,13 @@ const PriceUnit = styled.div`
 const SmallCardPreview = () => (
   <PreviewContainer>
     <PreviewImage />
-    <PreviewHeader>완전기이이이이이이이이이이이이이인 제목</PreviewHeader>
-    <PreviewAuctionContainer>
-      <PreviewDetailContainer float="left">
-        <DetailHeader>남은 시간</DetailHeader>
-        <DetailAuction
-          width="fit-content"
-          notebookwidth="fit-content"
-          fontsize="1.5rem"
-          notebookfontsize="1rem"
-          alignitems="end"
-        >
-          99:99:99
-        </DetailAuction>
-      </PreviewDetailContainer>
-      <PreviewDetailContainer float="right" marginleft="auto">
+    <DetailContainer>
+      <PreviewHeader>완전기이이dㄹㄴㄹㅇㅁㅇㄹㅁㄴㅇㄹㅁㄴㅇㄹ이이이인 제목</PreviewHeader>
+      <DeadLine>종료일 2023.07.23</DeadLine>
+      <PreviewDetailContainer float="right">
         <DetailHeader>현재가</DetailHeader>
         <DetailAuction
-          // width="8.813rem"
           width="fit-content"
-          // notebookwidth="6.5rem"
           notebookwidth="fit-content"
           fontsize="2rem"
           notebookfontsize="1.25rem"
@@ -149,7 +150,7 @@ const SmallCardPreview = () => (
           13,000<PriceUnit>원</PriceUnit>
         </DetailAuction>
       </PreviewDetailContainer>
-    </PreviewAuctionContainer>
+    </DetailContainer>
   </PreviewContainer>
 );
 
