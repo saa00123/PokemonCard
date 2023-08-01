40ec4679b7b9755a7cf2142260541354879c7781
Choejeonghun <saa00123@naver.com>
2023-08-01 22:07:27 +0900

    feat: env설정

--- src/firebase.js
+++ src/firebase.js
@@ -6,13 +6,13 @@ import "firebase/compat/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDSeQYLg-x2kPdHHEyA4W1ecYZeYLtodZU",
-  authDomain: "pokemon-card-auction-project.firebaseapp.com",
-  databaseURL: "https://pokemon-card-auction-project-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "pokemon-card-auction-project",
-  storageBucket: "pokemon-card-auction-project.appspot.com",
-  messagingSenderId: "397983741393",
-  appId: "1:397983741393:web:8d988893a0fa5ee5f39cf0",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
